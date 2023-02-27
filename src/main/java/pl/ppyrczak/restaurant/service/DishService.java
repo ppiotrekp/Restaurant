315170caa6fd7cb121acaee81f123a7951c57e5b
Piotr Pyrczak <ppyrczak5@gmail.com>
2023-02-27 13:06:07 +0100

    delete method done

--- src/main/java/pl/ppyrczak/restaurant/service/DishService.java
+++ src/main/java/pl/ppyrczak/restaurant/service/DishService.java
@@ -1,29 +1,37 @@
 package pl.ppyrczak.restaurant.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.ppyrczak.restaurant.model.Dish;
+import pl.ppyrczak.restaurant.repository.DishRepository;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DishService {
+    private final DishRepository dishRepository;
+
     public Dish addDish(Dish dish) {
-        return null;
+        return dishRepository.save(dish);
     }
 
     public List<Dish> getDishes() {
-        return null;
+        return dishRepository.findAll();
     }
 
     public Dish getDish(Long id) {
+        return dishRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Dish not found"));
+    }
+
+    public Dish editDish(Long id, Dish dishToUpdate) {
         return null;
     }
 
-    public Dish editDish(Long id) {
-        return null;
-    }
-
-    public Dish deleteDish(Long id) {
-        return null;
+    public void deleteDish(Long id) {
+        if (!dishRepository.findById(id).isPresent())
+            throw new RuntimeException("Dish does not exist");
+        dishRepository.deleteById(id);
     }
 }
